e3b5ee4fb67831295265e758362ad9b47824629c
Maksim <maxyarmoluk@gmail.com>
2026-08-09 12:16:00 +0300

    Доработка товаров...

--- src/com/dice/shop/repository/impl/GoodRepositoryImpl.java
+++ src/com/dice/shop/repository/impl/GoodRepositoryImpl.java
@@ -3,6 +3,8 @@ package com.dice.shop.repository.impl;
 import com.dice.shop.entity.good.Good;
 import com.dice.shop.entity.good.ProductCategory;
 import com.dice.shop.repository.GoodRepository;
+import com.dice.shop.util.SerializationUtil;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GoodRepositoryImpl implements GoodRepository {
@@ -11,28 +13,52 @@ public class GoodRepositoryImpl implements GoodRepository {
 
     @Override
     public void save(Good good) {
+        List<Good> goods = findAll();
+        goods.add(good);
+        SerializationUtil.serialize(goods, FILE_PATH);
     }
 
     @Override
     public List<Good> findAll() {
-        return null;
+        try {
+            return (List<Good>) SerializationUtil.deserialize(FILE_PATH);
+        } catch (RuntimeException e) {
+            return new ArrayList<>();
+        }
     }
 
     @Override
     public Good findById(Long id) {
-        return null;
+        return findAll()
+                .stream()
+                .filter(good -> good.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public List<Good> findByCategory(ProductCategory category) {
-        return null;
+        return findAll()
+                .stream()
+                .filter(good ->
+                        good.getCategory() == category)
+                .toList();
     }
 
     @Override
     public void update(Good good) {
+        List<Good> goods = findAll();
+        goods.removeIf(existingGood ->
+                existingGood.getId().equals(good.getId()));
+        goods.add(good);
+        SerializationUtil.serialize(goods, FILE_PATH);
     }
 
     @Override
     public void delete(Long id) {
+        List<Good> goods = findAll();
+        goods.removeIf(good ->
+                good.getId().equals(id));
+        SerializationUtil.serialize(goods, FILE_PATH);
     }
 }
